7941427b6f38d3c135b15a579e070e48e4c95fbb
gingerbraden <99813901+gingerbraden@users.noreply.github.com>
2023-03-11 16:03:17 +0100

    auto cropping working

--- app/src/main/java/com/example/pecscreator/CreateCardActivity.kt
+++ app/src/main/java/com/example/pecscreator/CreateCardActivity.kt
@@ -1,13 +1,23 @@
 package com.example.pecscreator
 
+import android.graphics.Bitmap
+import android.graphics.Rect
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import com.chaquo.python.PyObject
+import com.chaquo.python.Python
+import com.chaquo.python.android.AndroidPlatform
 import com.example.pecscreator.databinding.ActivityCreateCardBinding
-import com.example.pecscreator.ui.takephoto.CropPhoto
 import com.example.pecscreator.ui.takephoto.CreateCardViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import java.io.ByteArrayOutputStream
+import kotlin.streams.toList
 
 class CreateCardActivity : AppCompatActivity() {
 
@@ -16,31 +26,75 @@ class CreateCardActivity : AppCompatActivity() {
 
     private val viewModel: CreateCardViewModel by viewModels()
 
+    lateinit var module : PyObject
+
+    var coords : String = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityCreateCardBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        if (! Python.isStarted()) {
+            Python.start(AndroidPlatform(this))
+        }
+
+        val py = Python.getInstance()
+        module = py.getModule("opencv-scripts")
 
         val extras = intent.extras
         if (extras != null) {
             val uri: Uri? = intent.getParcelableExtra("uri")
             if (uri != null) {
                 viewModel.savedPhotoUri = uri
-                Log.d("AHOJ", "IDE TO")
+                viewModel.module = module
             }
-            //The key argument here must match that used in the other activity
         }
 
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, CropPhoto.newInstance(viewModel))
-                .commitNow()
+        binding.cropImageView.setImageUriAsync(viewModel.savedPhotoUri)
+
+
+
+        binding.button.setOnClickListener {
+
+            if (!coords.equals("")) {
+                val coords = coords.split(":").stream().map { x -> x.toInt() }.toList()
+                binding.cropImageView.cropRect = Rect(coords[0], coords[1], coords[0]+coords[2], coords[1]+coords[3])
+            }
+
         }
-
-
-
 
     }
+
+    override fun onStart() {
+        super.onStart()
+        getCroppedCoordinates()
+
+    }
+
+    fun getCroppedCoordinates() = run {
+        lifecycleScope.launch(Dispatchers.IO) {
+            delay(1500)
+            val bytes = module.callAttr("returnCoordinates",
+                getStringFromImageView(binding.cropImageView.getCroppedImage()!!))
+            coords = bytes.toString()
+            Log.d("ahoj", "hotovo koordinacky")
+            binding.button.isClickable = true
+            binding.button.alpha = 1F
+        }
+    }
+
+    suspend fun getStringFromImageView(b : Bitmap) : String {
+        val baos = ByteArrayOutputStream()
+        b.compress(Bitmap.CompressFormat.PNG, 10, baos)
+        val imageBytes = baos.toByteArray()
+        val encodedImage = android.util.Base64.encodeToString(imageBytes, android.util.Base64.DEFAULT)
+        return encodedImage
+    }
+
+
+
+
 
 
 
---
+++ app/src/main/python/opencv-scripts.py
@@ -0,0 +1,107 @@
+import cv2
+import numpy as np
+import os
+from os.path import join
+import io
+import base64
+from PIL import Image
+
+def getSaliencyMapSpectralResidual(x):
+
+    decoded_data = base64.b64decode(x)
+    np_data = np.fromstring(decoded_data, np.uint8)
+    img = cv2.imdecode(np_data, cv2.IMREAD_UNCHANGED)
+
+    saliency = cv2.saliency.StaticSaliencySpectralResidual_create()
+    (success, saliencyMap) = saliency.computeSaliency(img)
+    saliencyMap = (saliencyMap * 255).astype("uint8")
+    sal_img = Image.fromarray(saliencyMap)
+
+    buff = io.BytesIO()
+    sal_img.save(buff, format="PNG")
+    img_str = base64.b64encode(buff.getvalue())
+    return ""+str(img_str, 'utf-8')
+
+def getSaliencyMapFineGrained(x):
+
+   decoded_data = base64.b64decode(x)
+   np_data = np.fromstring(decoded_data, np.uint8)
+   img = cv2.imdecode(np_data, cv2.IMREAD_UNCHANGED)
+
+   saliency = cv2.saliency.StaticSaliencyFineGrained_create()
+   (success, saliencyMap) = saliency.computeSaliency(img)
+   saliencyMap = (saliencyMap * 255).astype("uint8")
+   sal_img = Image.fromarray(saliencyMap)
+
+   buff = io.BytesIO()
+   sal_img.save(buff, format="PNG")
+   img_str = base64.b64encode(buff.getvalue())
+   return ""+str(img_str, 'utf-8')
+
+def returnCropped(obrazok, bulin):
+
+    if not bulin:
+        decoded_data = base64.b64decode(getSaliencyMapFineGrained(obrazok))
+    else:
+        decoded_data = base64.b64decode(getSaliencyMapSpectralResidual(obrazok))
+    np_data = np.fromstring(decoded_data, np.uint8)
+    image = cv2.imdecode(np_data, cv2.IMREAD_UNCHANGED)
+
+    original = image.copy()
+    gray = image.copy()
+    blur = cv2.GaussianBlur(gray, (9,9), 0)
+    thresh = cv2.threshold(gray,0,255,cv2.THRESH_OTSU + cv2.THRESH_BINARY)[1]
+
+    # morphological transformations
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (2,2))
+    opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel) # remove noise
+    dilate_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (9,9))
+    dilate = cv2.dilate(opening, dilate_kernel, iterations=5)
+
+    cnts = cv2.findContours(dilate, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+    cnts = sorted(cnts, key=cv2.contourArea, reverse=True)
+
+    for c in cnts:
+        x,y,w,h = cv2.boundingRect(c)
+
+        decoded_data = base64.b64decode(obrazok)
+        np_data = np.fromstring(decoded_data, np.uint8)
+        ogimage = cv2.imdecode(np_data, cv2.IMREAD_UNCHANGED)
+
+        ROI = ogimage[y:y+h, x:x+w]
+        ROI = cv2.cvtColor(ROI, cv2.COLOR_BGR2RGB)
+        roi_img = Image.fromarray(ROI)
+        break
+
+    buff = io.BytesIO()
+    roi_img.save(buff, format="PNG")
+    img_str = base64.b64encode(buff.getvalue())
+    return ""+str(img_str, 'utf-8')
+
+
+def returnCoordinates(obrazok):
+    decoded_data = base64.b64decode(getSaliencyMapFineGrained(obrazok))
+    np_data = np.fromstring(decoded_data, np.uint8)
+    image = cv2.imdecode(np_data, cv2.IMREAD_UNCHANGED)
+
+    original = image.copy()
+    gray = image.copy()
+    blur = cv2.GaussianBlur(gray, (9,9), 0)
+    thresh = cv2.threshold(gray,0,255,cv2.THRESH_OTSU + cv2.THRESH_BINARY)[1]
+
+    # morphological transformations
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (2,2))
+    opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel) # remove noise
+    dilate_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (9,9))
+    dilate = cv2.dilate(opening, dilate_kernel, iterations=5)
+
+    cnts = cv2.findContours(dilate, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+    cnts = sorted(cnts, key=cv2.contourArea, reverse=True)
+
+    x,y,w,h = cv2.boundingRect(cnts[0])
+
+
+    return str(x) + ":" + str(y) + ":" + str(w) + ":" + str(h)
+
